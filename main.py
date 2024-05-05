9e0464829037ac4392faa27690f42249acf75cd6
Ishita <144627073+elekish@users.noreply.github.com>
2024-05-05 14:37:34 +0530

    Update main.py
    updated design

--- main.py
+++ main.py
@@ -7,44 +7,51 @@ from datetime import datetime, timedelta
 import json
 
 palm.configure(api_key=('AIzaSyDt0b1VBJqtVI_O18AtFDhyboTS8gOg_4g'))
-models = [m for m in palm.list_models() if 'generateText' in m.supported_generation_methods]
+models = [m for m in palm.list_models(
+) if 'generateText' in m.supported_generation_methods]
 model = models[0].name
-print(model)
+# print(model)
 
 # Streamlit app title and user input
-st.title("     :parachute: *Travel Itinerary Generator* :parachute:")
+st.markdown("<h1 style='text-align: center; text-weight:bold;'>Travel Itinerary Generator</h1>",
+            unsafe_allow_html=True)
+# st.title("     :parachute: Travel Itinerary Generator :parachute:")
 st.write('\n')
+st.image("travel2.jpg")
 city = st.text_input("Enter the location you're visiting :")
-start_date = st.date_input("Select the start date for your trip:", value=datetime.today())
+start_date = st.date_input(
+    "Select the start date for your trip:", value=datetime.today())
 
 # Set the maximum end date to 30 days after the start date
 max_end_date = start_date + timedelta(days=30)
 
 # User selects the end date of the trip
 end_date = st.date_input("Select the end date for your trip:",
-                         value=start_date + timedelta(days=1),  # Default to the next day
+                         # Default to the next day
+                         value=start_date + timedelta(days=1),
                          min_value=start_date,
                          max_value=max_end_date)
 
 # Calculate the number of days between start_date and end_date
-days = (end_date - start_date+timedelta(days=1)).days
-# days = st.number_input("Enter the number of days for your trip:", min_value=1, max_value=30)
+days = (end_date - start_date+timedelta(days=1))
+# days = st.number_input("Enter the number of days for your trip:",
+
 st.write('\n\n\n')
 # User preferences checkboxes
 st.write('\nChoose the type of attractions you prefer: ')
-checks=st.columns(6)
+checks = st.columns(6)
 with checks[0]:
-  restaurant = st.checkbox("Hotels")
+    restaurant = st.checkbox("Hotels")
 with checks[1]:
-  museums = st.checkbox("Museums")
+    museums = st.checkbox("Museums")
 with checks[2]:
-  outdoor = st.checkbox("Outdoor Activities")
+    outdoor = st.checkbox("Outdoor Activities")
 with checks[3]:
-  temple = st.checkbox("Temples")
+    temple = st.checkbox("Temples")
 with checks[4]:
-  kids_friendly = st.checkbox("Good for Kids")
+    kids_friendly = st.checkbox("Good for Kids")
 with checks[5]:
-  nature = st.checkbox("Nature")
+    nature = st.checkbox("Nature")
 
 # Transport mode options
 transport_modes = st.multiselect(
@@ -68,12 +75,11 @@ if st.button("Generate Itinerary"):
     if kids_friendly:
         prompt += " find famous places suitable for kids(only name),"
     if nature:
-        prompt += " discover famous beaches, mountains, waterfalls, caves or sand deserts(only name),"
+        prompt += " discover famous beaches, mountains, waterfalls,caves or sand deserts(only name),"
 
-
-
-    # prompt += ". Return the response in a properly formatted json string which can be imported in code using json.loads function in python."
-    prompt += r"""Limit the length of output json string to 2000 characters. Generate a structured JSON representation for the travel itinerary.
+    prompt += r"""Limit the length of output json string to 2000
+characters. Generate a structured JSON representation for the travel
+itinerary.
 
        {
   "days": [
@@ -122,15 +128,14 @@ if st.button("Generate Itinerary"):
   ]
 }
 
-        Ensure that each day has a 'day' field and a list of 'activities' with 'title', 'description', 'start_time', 'end_time', and 'location' fields. Keep descriptions concise.
+        Ensure that each day has a 'day' field and a list of
+'activities' with 'title', 'description', 'start_time', 'end_time',
+and 'location' fields. Keep descriptions concise.
 """
 
     import folium
     from streamlit_folium import folium_static
     from geopy.geocoders import Nominatim
-
-
-
 
     # Call the OpenAI API
     completion = palm.generate_text(
@@ -145,13 +150,13 @@ if st.button("Generate Itinerary"):
     itinerary = completion.result.strip()
     itinerary = itinerary[7:-3]
     # Display the itinerary from the JSON response
-    print(type(itinerary))
-    print(len(itinerary))
-    print(itinerary)
+    # print(type(itinerary))
+    print("Itinery length : ", len(itinerary))
+    # print(itinerary)
 
    # f = open(itinerary)
     itinerary_json = json.loads(itinerary)
-    print(itinerary_json)
+    # print(itinerary_json)
     # Initialize the map centered at a location
     import requests
 
@@ -162,13 +167,8 @@ if st.button("Generate Itinerary"):
     location = geocode_response['features'][0]['geometry']['coordinates']
     lng, lat = location[0], location[1]
 
-    #geolocator = Nominatim(user_agent="geoapiExercises")
-    #location = geolocator.geocode(hyderabad)
-
-
-
-
-
+    # geolocator = Nominatim(user_agent="geoapiExercises")
+    # location = geolocator.geocode(hyderabad)
 
     for day in itinerary_json["days"]:
         st.header(f"Day {day['day']}")
@@ -176,11 +176,12 @@ if st.button("Generate Itinerary"):
             st.subheader(activity["title"])
             st.write(f"Description: {activity['description']}")
             st.write(f"Location: {activity['location']}")
-            st.write(f"Time: {activity['start_time']} - {activity['end_time']}")
+            st.write(
+                f"Time: {activity['start_time']} - {activity['end_time']}")
             st.write(f"Link: {activity['link']}")
             st.write("\n")
 
-        m=folium.Map()
+        m = folium.Map()
         m = folium.Map(location=[lat, lng])
 
         # Create an empty list named 'places'
@@ -194,17 +195,19 @@ if st.button("Generate Itinerary"):
         # Add markers for the places
         for place in places:
             address = place + " " + city
-            url = f'https://api.geoapify.com/v1/geocode/search?text={address}&apiKey={api_key}'
+            url =f'https://api.geoapify.com/v1/geocode/search?text={address}&apiKey={api_key}'
             geocode_response = requests.get(url).json()
             if geocode_response['features']:
-              location = geocode_response['features'][0]['geometry']['coordinates']
-              lng1, lat1 = location[0], location[1]
-            # location = geolocator.geocode(place)
-              folium.Marker([lat1, lng1], popup=address, icon=folium.Icon(color='red')).add_to(m)
+                location =geocode_response['features'][0]['geometry']['coordinates']
+                lng1, lat1 = location[0], location[1]
+                # location = geolocator.geocode(place)
+                folium.Marker([lat1, lng1], popup=address,
+                              icon=folium.Icon(color='red')).add_to(m)
 
         # Connect the places with lines (assuming travel by road)
-        # folium.PolyLine(locations=[(geolocator.geocode(place).latitude, geolocator.geocode(place).longitude) for place in
-        #                          activity["title"]], color='blue').add_to(m)
+        # folium.PolyLine(locations=[(geolocator.geocode(place).latitude,
+        #for place in geolocator.geocode(place).longitude)
+        #                            activity["title"]], color='blue').add_to(m)
 
         # Initialize an empty list to store the coordinates
         coordinates = []
@@ -212,18 +215,19 @@ if st.button("Generate Itinerary"):
         # Get the coordinates for each place
         for place in places:
             address = place + " " + city
-            url = f'https://api.geoapify.com/v1/geocode/search?text={address}&apiKey={api_key}'
+            url =f'https://api.geoapify.com/v1/geocode/search?text={address}&apiKey={api_key}'
             geocode_response = requests.get(url).json()
             if geocode_response['features']:
-              location = geocode_response['features'][0]['geometry']['coordinates']
-              lng2, lat2 = location[0], location[1]
-              coordinates.append((lat2, lng2))  # Append the coordinates to the list
+                location =geocode_response['features'][0]['geometry']['coordinates']
+                lng2, lat2 = location[0], location[1]
+                # Append the coordinates to the list
+                coordinates.append((lat2, lng2))
 
         # Add the PolyLine to the map
         folium.PolyLine(locations=coordinates[:-1], color='black').add_to(m)
 
-        # sw=coordinates.min().values.tolist()
-        # ne=coordinates.max().values.tolist()
+        # sw = coordinates.min().values.tolist()
+        # ne = coordinates.max().values.tolist()
         m.fit_bounds(coordinates)
 
         st.write('\n')
@@ -233,7 +237,6 @@ if st.button("Generate Itinerary"):
 
     # Set the start date to tomorrow
     start_date = datetime.now() + timedelta(days=1)
-
 
     def get_download_link(content, filename):
         """Generates a download link for the given content."""
@@ -252,21 +255,45 @@ if st.button("Generate Itinerary"):
             event.name = activity.get("title", "")
             event.description = activity.get("description", "")
             event.location = activity.get("location", "")
-            print(event.location)
-            print("start_date:", start_date)
-            print("end_time:",
-                  start_date + timedelta(days=day - 1, hours=int(activity.get("end_time", "00:00").split(":")[0]),
-                                         minutes=int(activity.get("end_time", "00:00").split(":")[1][:2])))
-            event.begin = start_date + timedelta(days=day - 1,
-                                                 hours=int(activity.get("start_time", "00:00").split(":")[0]),
-                                                 minutes=int(activity.get("start_time", "00:00").split(":")[1][:2]))
-            event.end = start_date + timedelta(days=day, hours=int(activity.get("end_time", "00:00").split(":")[0]),
-                                               minutes=int(activity.get("end_time", "00:00").split(":")[1][:2]))
+            # print(event.location)
+            # print("start_date:", start_date)
+            # print("end_time:",
+            #       start_date + timedelta(days=day - 1,
+            #hours=int(activity.get("end_time", "00:00").split(":")[0]),
+
+            #minutes=int(activity.get("end_time", "00:00").split(":")[1][:2])))
+            event.begin = start_date + timedelta(days=day - 1,hours=int(activity.get(
+                                                     "start_time","00:00").split(":")[0]),minutes=int(activity.get("start_time", "00:00").split(":")[1][:2]))
+            event.end = start_date + timedelta(days=day,hours=int(activity.get("end_time", "00:00").split(":")[0]),minutes=int(activity.get("end_time", "00:00").split(":")[1][:2]))
             cal.events.add(event)
 
-
-    cal_content = str(cal)  # Use str to obtain the serialized iCalendar content
+    # Use str to obtain the serialized iCalendar content
+    cal_content = str(cal)
 
     # Create a download link
     st.success("Itinerary ready to export!")
-    st.markdown(get_download_link(cal_content, "Itinerary.ics"), unsafe_allow_html=True)
+    st.markdown(get_download_link(cal_content, "Itinerary.ics"),unsafe_allow_html=True)
+
+
+# Footer
+
+footer = """<style>
+.footer {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  background-color: #29293d;
+  color: #8c8c8c;
+  text-align: center;
+}
+
+.pad-top {
+    padding-top : 1.2em;
+}
+</style>
+<div class="footer">
+<p class="pad-top">Copyright © 2024 by Ishita Biswas and Arya Kanta. All rights reserved.</p>
+</div>
+"""
+st.markdown(footer, unsafe_allow_html=True)
